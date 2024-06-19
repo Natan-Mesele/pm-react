cd324b404272867e2123c039761e6094690a2f28
Natan-Mesele <huluale12@gmail.com>
2024-06-19 23:02:01 +0300

    issue

--- src/Redux/Chat/ActionType.js
+++
@@ -1,3 +0,0 @@
-export const FETCH_MESSAGES_REQUEST ="";
-export const FETCH_MESSAGES_SUCCESS="";
-export const FETCH
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -4,12 +4,16 @@ import {
   legacy_createStore,
 } from "@reduxjs/toolkit";
 import { thunk } from "redux-thunk";
-import { authReducer } from "../Auth/Reducer";
-import { projectReducer } from "../project/Reducer";
+import { authReducer } from "./Auth/Reducer";
+import { projectReducer } from "./project/Reducer";
+import ChatReducer from "./Chat/Reducer";
+import commentReducer from "./comment/Reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
+  Chat: ChatReducer,
+  comment: commentReducer
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
